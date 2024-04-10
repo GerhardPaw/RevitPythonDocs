bda23a265492204b3512356a42914365f4c16f47
GerhardPaw <97836426+GerhardPaw@users.noreply.github.com>
2024-04-10 16:49:55 +0200

    Create ViewportFromView_basic_ironpython_cpython_2022_2023_2024.py

---
+++ Dynamo/ViewportFromView_basic_ironpython_cpython_2022_2023_2024.py
@@ -0,0 +1,22 @@
+import clr
+clr.AddReference("RevitNodes")
+clr.AddReference("RevitServices")
+
+import Revit
+clr.ImportExtensions(Revit.Elements)
+
+from RevitServices.Persistence import DocumentManager
+from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory
+
+doc = DocumentManager.Instance.CurrentDBDocument
+
+viewport_dict = {vp.ViewId: vp for vp in FilteredElementCollector(doc)
+                 .OfCategory(BuiltInCategory.OST_Viewports)
+                 .WhereElementIsNotElementType()
+                 .ToElements()}
+
+views = UnwrapElement(IN[0]) if isinstance(IN[0], list) else [UnwrapElement(IN[0])]
+
+matching_viewports = [viewport_dict.get(view.Id) for view in views if view.Id in viewport_dict]
+
+OUT = matching_viewports
